ace30a451e5f2c613193940de720f6b9d5ef93f3
brandsignlafrica-ctrl <brandsignlafrica@gmail.com>
2026-06-04 13:41:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const app = express();
 
 console.log("ENTRY FILE IS EXECUTING");
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    console.log("ROOT HIT");
-    res.status(200).send("Server working");
+  console.log("ROOT HIT");
+  res.status(200).send("Server working");
+});
+
+app.get('/health', (req, res) => {
+  console.log("HEALTH HIT");
+  res.status(200).send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log("SERVER STARTED ON PORT " + PORT);
+  console.log("SERVER STARTED ON PORT " + PORT);
 });
